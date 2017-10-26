eb34b2173e508828ff6ecc04919dc0dc56a52108
julielinx <jfisher12@gmail.com>
2017-10-26 18:37:02 -0500

    algorithm run
    algorithm has been run, pends info re: how to view the results

--- firstRunThrough.R
+++ firstRunThrough.R
@@ -8,4 +8,5 @@ digits.nolabelsm <- digits.train[1:100,2:785]
 digits.small <- digits.train[1:100,]
 str(digits.nolabelsm)
 digits.cluster <- specc(digits.nolabelsm, centers = 10)
+digits.cluster
 table()
