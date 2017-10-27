0d19cdf03748cb6a4980231f8e474ed0f0872a3c
julielinx <jfisher12@gmail.com>
2017-10-26 19:32:32 -0500

    R update

--- firstRunThrough.R
+++ firstRunThrough.R
@@ -8,5 +8,17 @@ digits.nolabelsm <- digits.train[1:100,2:785]
 digits.small <- digits.train[1:100,]
 str(digits.nolabelsm)
 digits.cluster <- specc(digits.nolabelsm, centers = 10)
-digits.cluster
-table()
+digits.cluster$centers
+
+#
+plot(dataset, col=sluter.set)
+
+size(digits.cluster) #membership per cluster
+withinss(digits.cluster)
+kernelf(digits.cluster)
+digits.cluster[1:784] # It clustered for the values in the columns instead of as a row = a variable
+
+class(digits.nolabelsm)
+
+trial2 <- specc(digits.nolabelsm, data=digits.nolabelsm, centers=10)
+trial2[1:784]
