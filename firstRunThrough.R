e32f25c0ccea3c617a000fabf16d11977ec88b7e
julielinx <jfisher12@gmail.com>
2017-10-26 20:15:02 -0500

    as a matrix
    the code is messy and I'm not fixing it tonight. But convert the dataframe to a matrix and it runs based on the rows as it's supposed to

--- firstRunThrough.R
+++ firstRunThrough.R
@@ -1,14 +1,15 @@
 library(kernlab)
+setwd("~/Learning/Lipscomb MSDS/Data Mining/Final _Project")
 digits.train <- read.csv('MNIST/train.csv')
 digits.test <- read.csv('MNIST/test.csv')
 head(digits.train)
 str(digits.train)
 test <- digits.train[1:100,2:785]
-digits.nolabelsm <- digits.train[1:100,2:785]
+digits.nolabelsm <- as.matrix(digits.train[1:100,2:785])
 digits.small <- digits.train[1:100,]
 str(digits.nolabelsm)
 digits.cluster <- specc(digits.nolabelsm, centers = 10)
-digits.cluster$centers
+digits.cluster
 
 #
 plot(dataset, col=sluter.set)
@@ -16,9 +17,11 @@ plot(dataset, col=sluter.set)
 size(digits.cluster) #membership per cluster
 withinss(digits.cluster)
 kernelf(digits.cluster)
-digits.cluster[1:784] # It clustered for the values in the columns instead of as a row = a variable
+digits.cluster[1:100] # It clustered for the values in the columns instead of as a row = a variable
 
 class(digits.nolabelsm)
 
 trial2 <- specc(digits.nolabelsm, data=digits.nolabelsm, centers=10)
 trial2[1:784]
+
+digits.nolabelsm <- as.matrix(digits.nolabelsm)
